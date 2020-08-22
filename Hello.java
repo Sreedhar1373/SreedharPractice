bc6cfc75438b275934dfea4b257f64945efb50e6
Sreedhar <sreedhar.lalapeta@gmail.com>
2020-08-22 18:10:55 +0530

    addding a somwya print line

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ class Hello
 public static void main (String args[])
 {
 System.out.println("Java Hello World");
-System.out.println("Hello sreedhar");
+System.out.println("Hello Sreedhar");
+System.out.println("Hello Sowmya");
+
 }
 }
